49050697830291cf3a6f4f5ed4c785004b749b24
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2021-03-09 16:56:54 +0200

    commit 09/03/2021 4

--- js/header.js
+++ js/header.js
@@ -1,18 +1,7 @@
 $(document).ready(function() {
     $("#addNew").click(function(){
         $("#tableManager").show();
-        $("#editRowID").val(""),
-        $("#Fname").val(""),
-        $("#Lname").val(""),
-        $("#email").val(""),
-        $("#username").val("");
-        $("#password").val("");
-        $("#confirm").val("");
-        document.getElementById('window_label').innerHTML = "Εισαγωγή Νέου Χρήστη"; 
         document.getElementById("Fname").focus();
-        $("#manageBtn").attr("value", "Αποθήκευση").attr("onclick", "ManageData('addNew')");
-        $("#username").removeAttr("disabled", "disabled");
-        $("#email").removeAttr("disabled", "disabled");
     });
     $("#close").click(function(){
         $("#tableManager").hide();
